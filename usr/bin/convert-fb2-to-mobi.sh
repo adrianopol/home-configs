d13bd71f2e0484e5f7070718a1224103cf930587
Andrew Petelin <adrianopol@gmail.com>
2018-08-01 19:10:58 +0300

    convert-fb2-to-mobi.sh: determine number of threads.

--- usr/bin/convert-fb2-to-mobi.sh
+++ usr/bin/convert-fb2-to-mobi.sh
@@ -4,6 +4,9 @@ set -eux
 
 path="${1:?Need directory to search in.}"
 
+ncores="$(grep -m1 'cpu cores' /proc/cpuinfo | grep -Po '\d+')"
+[[ "$ncores" -ge 1 ]] || ncores=1
+
 find "$path" -name "*.fb2*" -print0 | \
   sort | \
   parallel -j4 -r0 --plus \
