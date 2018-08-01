68e3a97fabed5061679c5432f8be672b31581760
Andrew Petelin <adrianopol@gmail.com>
2018-08-01 19:21:03 +0300

    convert-fb2-to-mobi.sh: added usage.

--- usr/bin/convert-fb2-to-mobi.sh
+++ usr/bin/convert-fb2-to-mobi.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
-path="${1:?Need directory to search in.}"
+PROGNAME="$(readlink -f "$(basename "$0")")"
+
+usage() {
+  echo "This script will convert all fb2 files to mobi format using ebook-convert utility"
+  echo "from calibre package."
+  echo
+  echo "Usage:"
+  echo
+  echo "  $PROGNAME <directory>"
+  echo
+  echo "where <directory> is a starting point to search .fb2 or .fb2.zip files."
+}
+
+path="${1:-}"
+
+if [[ -z $path || "$path" == "-h" || "$path" == "--help" ]]; then
+  usage
+  exit
+fi
 
 ncores="$(grep -m1 'cpu cores' /proc/cpuinfo | grep -Po '\d+')"
 [[ "$ncores" -ge 1 ]] || ncores=1
 
-find "$path" -name "*.fb2*" -print0 | \
+set -x
+find "$path" \( -name "*.fb2" -o -name '*.fb2.zip' \) -print0 | \
   sort | \
   parallel -j"$ncores" -r0 --plus \
     'ebook-convert {} {..}.mobi --mobi-ignore-margins --mobi-keep-original-images'
