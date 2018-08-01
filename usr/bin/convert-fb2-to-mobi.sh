d773720051d70cc038f5ef1ac7fa1a68264f23a2
Andrew Petelin <adrianopol@gmail.com>
2018-08-01 19:12:18 +0300

    small update to prev commit

--- usr/bin/convert-fb2-to-mobi.sh
+++ usr/bin/convert-fb2-to-mobi.sh
@@ -9,5 +9,5 @@ ncores="$(grep -m1 'cpu cores' /proc/cpuinfo | grep -Po '\d+')"
 
 find "$path" -name "*.fb2*" -print0 | \
   sort | \
-  parallel -j4 -r0 --plus \
+  parallel -j"$ncores" -r0 --plus \
     'ebook-convert {} {..}.mobi --mobi-ignore-margins --mobi-keep-original-images'
