8708677a35897be45767ea38fe2c3c302aec23a6
Andrew Petelin <adrianopol@gmail.com>
2020-12-20 19:10:41 +0300

    updates

--- .vimrc
+++ .vimrc
@@ -123,7 +123,7 @@ if has('gui_running')
   set guioptions-=T
   set guioptions-=l
   set guioptions-=L
-  set guioptions-=r
+  "set guioptions-=r
   set guioptions-=R
 
   map <S-Insert> <MiddleMouse>
--- bin/convert-phone-video.sh
+++ bin/convert-phone-video.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 # Usage:
 #
 #   convert-phone-video.sh [ result_path ] file1 ...
+
+scale=y
 
 dest_prefix=
 if [[ -d "${1:-}" ]]; then
@@ -15,17 +17,28 @@ fi
 for path in "$@"; do
   file="$(basename "$path")"
   ext=webm
-  out_path="${path%.*}.$ext"
   if [[ -n "$dest_prefix" ]]; then
     out_path="$dest_prefix/${file%.*}.$ext"
+  else
+    out_path="$dest_prefix/${path%.*}.$ext"
   fi
-  set -x
-  time ffmpeg -i "$path" \
-    -vf scale=w=1200:h=1000:force_original_aspect_ratio=decrease \
-    -c:v libvpx -b:v 2000k \
-    -c:a libvorbis -b:a 64k \
-    -threads 4 \
+  args=(
+    -i "$path"
+    -c:v libvpx -b:v 2000k
+    -c:a libvorbis -b:a 64k
+    -threads 4
+  )
+  if [[ "$scale" == y ]]; then
+    #TODO: perofrm scaling only when dimensions are too large
+    args+=(
+      -vf scale=w=1400:h=1200:force_original_aspect_ratio=decrease
+    )
+  fi
+  args+=(
     "$out_path"
+  )
+  set -x
+  time ffmpeg "${args[@]}"
   set +x
   echo
   echo
